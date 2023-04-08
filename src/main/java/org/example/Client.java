01dde3d3663e3eb4a857223127ed7ef64bbf2a49
Evgeniy Leschenko <melbourne333@mail.ru>
2023-04-08 19:21:36 +1000

    0.0.2

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -2,23 +2,38 @@ package org.example;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
-        try{
-            Socket socket = new Socket("127.0.0.1",8189);
+        try {
+            Socket socket = new Socket("127.0.0.1", 8189);
             DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            String s = in.readUTF();
+                            System.out.println(s);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            });
+            thread.setDaemon(true);
+            thread.start();
             Scanner scanner = new Scanner(System.in);
-            while (true){
+            while (true) {
                 String s = scanner.nextLine();
                 out.writeUTF(s);
-                String fromServer = in.readUTF();
-                System.out.println(fromServer);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
